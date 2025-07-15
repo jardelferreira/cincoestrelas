bc51c8d92a2047149c96c2cb7b0d715c439d08be
jardelferreira <jardelferreira@gmail.com>
2025-07-15 08:42:11 -0300

    first commit

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "pwa-tabela-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./styles.css",
+  "./spinner.png",
+  "./operacoes.js",
+  "./script.js",
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
