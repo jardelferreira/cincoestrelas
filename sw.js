6ac64069672b7548e6d00851f900357481506904
jardelferreira <jardelferreira@gmail.com>
2025-07-19 20:55:54 -0300

    gerenciador

--- sw.js
+++ sw.js
@@ -12,15 +12,38 @@ const urlsToCache = [
     `https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/spinner.png`,
     `https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/operacoes.js`,
     `https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/script.js`,
+    `https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/webfonts/fa-brands-400.ttf`,
+    `https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/webfonts/fa-brands-400.woff2`,
+    `https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/webfonts/fa-regular-400.ttf`,
+    `https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/webfonts/fa-regular-400.woff2`,
+    `https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/webfonts/fa-solid-900.ttf`,
+    `https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/webfonts/fa-solid-900.woff2`,
+    `https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/webfonts/fa-v4compatibility.ttf`,
+    `https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/webfonts/fa-v4compatibility.woff2`,
     'https://cdn.jsdelivr.net/gh/jardelferreira/cincoestrelas@main/src/output.css'
 
 ];
 
 self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME).then(cache => {
+            return Promise.all(
+                urlsToCache.map(url =>
+                    fetch(url).then(response => {
+                        if (!response.ok) {
+                            console.warn("⚠️ Falha ao buscar:", url, response.status);
+                            throw new Error(`Erro ao buscar ${url}`);
+                        }
+                        return cache.put(url, response.clone());
+                    }).catch(err => {
+                        console.error("❌ Erro ao adicionar ao cache:", url, err);
+                    })
+                )
+            );
+        })
     );
 });
+
 
 self.addEventListener("activate", event => {
     event.waitUntil(
